aedfc3eb099e463ddded330e186938498cb121f5
Alex <alexandertaylor315@gmail.com>
2022-09-09 15:51:03 -0400

    attempt using more functions pt2

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ is_user_satisfied = "No"
 
 def greeting():
     print("Welcome to your vacation day trip advisor! Below you will find a randomly selected location, restaurant, method of transportation, and form of entertainment for your day trip.")
-
-def randomly_generated_location(vacation_locations):
+def randomly_generated_location():
     selected_location = random.choice(vacation_locations)
     return selected_location
-
-def randomly_generated_restaurant(restaurants):
+def randomly_generated_restaurant():
     selected_restaurant = random.choice(restaurants)
     return selected_restaurant
-
-def randomly_generated_transportation(transportation_list):
+def randomly_generated_transportation():
     selected_transportation = random.choice(transportation_list)
     return selected_transportation
-
-def randomly_generated_entertainment(entertainment_list):
+def randomly_generated_entertainment():
     selected_entertainment = random.choice(entertainment_list)
     return selected_entertainment
+greeting()
+selected_location = randomly_generated_location()
+selected_restaurant = randomly_generated_restaurant()
+selected_transportation = randomly_generated_transportation()
+selected_entertainment = randomly_generated_entertainment()
 
 while is_user_satisfied == "No":
     print(f"Location: {selected_location} - Restaurant: {selected_restaurant} - Transportation: {selected_transportation} - Enetertainment: {selected_entertainment}")
